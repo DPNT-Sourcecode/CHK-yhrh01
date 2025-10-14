1f8d7aaa26faf6a7aba078937bf2cbbfe13d72fc
sbx_user1051 <sbx_user1051@169.254.12.41>
2025-10-14 19:39:09 +0000

    Tue Oct 14 19:39:09 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,19 @@
+PRICES = {
+    'A': 50,
+    'B': 30,
+    'C': 20,
+    'D': 15
+}
+
+OFFERS = {
+    'A': {
+        ''
+    }
+}
 
 class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
         raise NotImplementedError()
+
