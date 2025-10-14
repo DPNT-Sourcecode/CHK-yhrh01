e878bbe10b3ff3058c36e54c1819106842dae6ea
sbx_user1051 <sbx_user1051@169.254.12.41>
2025-10-14 20:06:09 +0000

    Tue Oct 14 20:06:09 UTC 2025

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,1 +1,11 @@
-from lib.solutions.
+from lib.solutions.CHK.checkout_solution import CheckoutSolution
+
+class TestCheckout():
+
+    def test_single_sku(self):
+        basket = "A"
+        assert CheckoutSolution().checkout(basket) == 50
+
+    def test_single_sku(self):
+        basket = "A"
+        assert CheckoutSolution().checkout(basket) == 50
