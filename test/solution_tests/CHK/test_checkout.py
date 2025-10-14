00e10776e0545adea94c2a2943fed5de06bd5a7f
sbx_user1051 <sbx_user1051@169.254.12.41>
2025-10-14 20:09:09 +0000

    Tue Oct 14 20:09:09 UTC 2025

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -6,6 +6,20 @@ class TestCheckout():
         basket = "A"
         assert CheckoutSolution().checkout(basket) == 50
 
-    def test_single_sku(self):
-        basket = "A"
-        assert CheckoutSolution().checkout(basket) == 50
+    def test_single_sku_with_multiple_count(self):
+        basket = "AA"
+        assert CheckoutSolution().checkout(basket) == 100
+
+    def test_several_skus(self):
+        basket = "AB"
+        assert CheckoutSolution().checkout(basket) == 80
+
+    def test_skus_with_offer_no_remainder(self):
+        basket = "AAA"
+        assert CheckoutSolution().checkout(basket) == 130
+
+    def test_skus_with_offer_with_remainder(self):
+        basket = "AAAA"
+        assert CheckoutSolution().checkout(basket) == 180
+
+    def test_skus_with_several_offers
